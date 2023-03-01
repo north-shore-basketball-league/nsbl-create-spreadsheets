319b672f8d04c26eaa3df61800bb142cf595a04a
tobsterclark <me@tobyclark.dev>
2023-03-01 21:38:15 +1100

    starting to use xlwings for sheet copying

--- exportSpreadsheet.py
+++ exportSpreadsheet.py
@@ -1,3 +1,4 @@
+import io
 import os
 import uuid
 from openpyxl import Workbook, load_workbook
@@ -6,8 +7,9 @@ from openpyxl.utils.cell import coordinate_from_string, column_index_from_string
 from openpyxl.drawing.image import Image as OpenpyxlImage
 from copy import copy
 from PIL import Image as PILImage
+import xlwings as xw
 import re
-from time import sleep
+import pathlib
 
 # TODO:
 # Make times 12 hour
@@ -144,82 +146,25 @@ class ExportSpreadsheets:
         return None
 
     def _create_worksheet(self, type, name):
-        worksheet = self.workbook.create_sheet(name)
+        self.workbook.create_sheet(name)
 
-        template = self.template[type]
+        self.workbook.save(self.fileName)
+        self.workbook.close()
 
-        self._copy_sheet(template, worksheet, type)
+        parentPath = pathlib.Path(__file__).parent.resolve()
 
-    def _copy_sheet(self, source_sheet, target_sheet, type):
-        # copy all the cel values and styles
-        self._copy_cells(source_sheet, target_sheet)
-        self._copy_sheet_attributes(source_sheet, target_sheet, type)
+        templateWb = xw.Book(str(parentPath)+"\\template.xlsx")
+        outputWb = xw.Book(str(parentPath)+"\\"+self.fileName)
 
-    def _copy_sheet_attributes(self, source_sheet, target_sheet, type):
-        target_sheet.sheet_format = copy(source_sheet.sheet_format)
-        target_sheet.sheet_properties = copy(source_sheet.sheet_properties)
-        target_sheet.merged_cells = copy(source_sheet.merged_cells)
-        target_sheet.page_margins = copy(source_sheet.page_margins)
-        target_sheet.freeze_panes = copy(source_sheet.freeze_panes)
+        templateWb.sheets[type].copy(before=outputWb.sheets[name])
 
-        target_sheet._images = []
+        print(name)
 
-        # Save the NSBL img on the template sheet
-        if type == "scoresheet" and self.scoresheetImg == None:
-            imgs = copy(source_sheet._images)
+        outputWb.save()
+        outputWb.close()
+        templateWb.close()
 
-            for img in imgs:
-                self.scoresheetImg = PILImage.open(img.ref)
-                self.scoresheetImg.resize((img.width, img.height))
-        elif type == "scoresheet":
-            target_sheet._images = [self.scoresheetImg]
-
-        # set row dimensions
-        # So you cannot copy the row_dimensions attribute. Does not work (because of meta data in the attribute I think). So we copy every row's row_dimensions. That seems to work.
-        for rn in range(len(source_sheet.row_dimensions)):
-            target_sheet.row_dimensions[rn] = copy(
-                source_sheet.row_dimensions[rn])
-
-        if source_sheet.sheet_format.defaultColWidth is None:
-            print('Unable to copy default column wide')
-        else:
-            target_sheet.sheet_format.defaultColWidth = copy(
-                source_sheet.sheet_format.defaultColWidth)
-
-        # set specific column width and hidden property
-        # we cannot copy the entire column_dimensions attribute so we copy selected attributes
-        for key, value in source_sheet.column_dimensions.items():
-            # Excel actually groups multiple columns under 1 key. Use the min max attribute to also group the columns in the targetSheet
-            target_sheet.column_dimensions[key].min = copy(
-                source_sheet.column_dimensions[key].min)
-            # https://stackoverflow.com/questions/36417278/openpyxl-can-not-read-consecutive-hidden-columns discussed the issue. Note that this is also the case for the width, not onl;y the hidden property
-            target_sheet.column_dimensions[key].max = copy(
-                source_sheet.column_dimensions[key].max)
-            target_sheet.column_dimensions[key].width = copy(
-                source_sheet.column_dimensions[key].width)  # set width for every column
-            target_sheet.column_dimensions[key].hidden = copy(
-                source_sheet.column_dimensions[key].hidden)
-
-    def _copy_cells(self, source_sheet, target_sheet):
-        for (row, col), source_cell in source_sheet._cells.items():
-            target_cell = target_sheet.cell(column=col, row=row)
-
-            target_cell._value = source_cell._value
-            target_cell.data_type = source_cell.data_type
-
-            if source_cell.has_style:
-                target_cell.font = copy(source_cell.font)
-                target_cell.border = copy(source_cell.border)
-                target_cell.fill = copy(source_cell.fill)
-                target_cell.number_format = copy(source_cell.number_format)
-                target_cell.protection = copy(source_cell.protection)
-                target_cell.alignment = copy(source_cell.alignment)
-
-            if source_cell.hyperlink:
-                target_cell._hyperlink = copy(source_cell.hyperlink)
-
-            if source_cell.comment:
-                target_cell.comment = copy(source_cell.comment)
+        self.workbook = load_workbook(self.fileName)
 
 
 if __name__ == "__main__":
